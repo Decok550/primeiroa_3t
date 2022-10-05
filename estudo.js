212cae5497e186d120a65e5dd829825744ea1d27
Decok550 <108940285+Decok550@users.noreply.github.com>
2022-10-05 08:20:23 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,8 +1,6 @@
 let tabuada = 9;
-let nome = "André"
+
 function escreva(){
-    //2 x 1=
-    document.write("tabuada do" + tabuada+<br>");
     document.write(tabuada + " x 1 ="+(tabuada*1)+"<br>");
     document.write(tabuada + " x 2 ="+(tabuada*2)+"<br>");
     document.write(tabuada + " x 3 ="+(tabuada*3)+"<br>");
@@ -13,5 +11,23 @@ function escreva(){
     document.write(tabuada + " x 8 ="+(tabuada*8)+"<br>");
     document.write(tabuada + " x 9 ="+(tabuada*9)+"<br>");
     document.write(tabuada + " x 10 ="+(tabuada*10)+"<br>");
-    document.write("feito por" + nome);
+   
+
+
+function minhaTabuada{
+    for(let i = 0; <= 10; i++){
+        document.write("Tabuada do " + i + "<br>")
+       for(let j = 1; j <= 10 ; j++){
+          document.write(i +" x " + j + " = " +(j*i)+"<br>")
+       }
+       document.write("<br>");
+    }
+    
+  }
+
+function quadrado() {
+   for(let i = 1; i <= 20; i++)
+   document.write("O quadrado de" +i + "é" + (i*i)+"<br>)
+    
 }
+
